564b3fd4d68ad317b87a56cff412ef5279ff73d6
thanhluong <doannguyenthanhluong@gmail.com>
2020-08-11 22:42:20 +0900

    Initialize index page

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+    console.log("App is listening on port " + listener.address().port);
+});
